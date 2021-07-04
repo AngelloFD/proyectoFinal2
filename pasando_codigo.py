eba2e72bcafe83de60c6878d99d6f57eab35000d
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2021-07-04 12:46:13 -0500

    Corrección de Lista
    Corrigiendo la impresión de la lista, siendo este una lista y no un Array.

--- pasando_codigo.py
+++ pasando_codigo.py
@@ -1,11 +1,11 @@
 letras = "ABCDE"
-Problem[5,5]
+Problem[]
 
 print()
 print("    ListaM  ")
-print("  1 2 3 4 5")
-for x in range(1,5):
-    print(letras[x,x], end="")
-    for i in range(1,5):
-        print(Problem[x,i], end="")
+print("    1  2  3  4  5")
+for x in range(5):
+    print(letras[x], " - ", end="")
+    for i in range(5):
+        print(Problem[i], " ", end="")
     print()
