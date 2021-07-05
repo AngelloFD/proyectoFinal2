7005baf54f08e3d5dcf8e65aaa74016173ea24cd
vivianjaime <vivian.jaime@usil.pe>
2021-07-05 12:31:19 -0500

    Update recibir respuesta.py

--- recibir respuesta.py
+++ recibir respuesta.py
@@ -1,13 +1,20 @@
-#Validar letra and número
-while True:
+def es_letra_o_numero(comando):
+    let = comando[0]
+    num = comando[1]
+    check = any(str.isdigit(let) for char in let)
+    if check == False:
+        check = any(str.isdigit(num) for char in num)
+        if check == True:
+            return True
+        else:
+            return False
+    else:
+        return False
 
-    print ("Digitar letra y numero")
-Digitar = str(input())
-print('First 5 caracthers: ', Digitar)
- 
 
-if (Digitar):
-    print('Ha digitado un movimiento valido. ')
 
-else:
-    print('El valor digitado no corresponde con un movimiento valido. ')
+
+a = input("Ingrese una letra y un numero: ")
+print(a[0])
+print(a[1])
+print(es_letra_o_numero(a))
